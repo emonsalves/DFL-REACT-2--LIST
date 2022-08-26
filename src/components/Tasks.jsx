551b13c5d86975624a25521e0bbd6df3923e5cfc
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-08-26 15:28:44 -0400

    Update : Activity 2.5 Study Guide

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react"
+import { tasksList } from "./ArrayData"
 
 const Tasks = () => {
-  const tasksList = ["Task 1", "Task 2", "Task 3"]
-  const [arrayTasks, setArrayTasks] = useState(...[tasksList])
+  const [arrayTasks, setArrayTasks] = useState(tasksList)
   const [nameTask, setNameTask] = useState("")
 
   const styleInput =
-    "p-2 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    "m-2 p-2 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
   const styleButton =
-    "py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+    "py-2.5 px-5 ml-2 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
 
   const submitForm = (e) => {
     e.preventDefault(),
-      console.log("Form Send"),
-      setArrayTasks([...arrayTasks, nameTask]),
+      setArrayTasks([...arrayTasks, { name: nameTask, complete: false }]),
       setNameTask("")
-    console.table(arrayTasks)
+  }
+
+  const captureTask = (e) => {
+    setNameTask(e.target.value)
+  }
+
+  const uploadTask = (task) => {
+    console.log(task)
+    const newArrayTasks = [...arrayTasks]
+    const index = newArrayTasks.findIndex((ele) => ele.name === task.name)
+    console.log(index, task.name)
+
+    newArrayTasks[index].complete = true
+    setArrayTasks(newArrayTasks)
   }
 
   return (
@@ -25,7 +37,7 @@ const Tasks = () => {
           className={styleInput}
           type="text"
           placeholder="Insert your Task"
-          onChange={(e) => setNameTask(e.target.value)}
+          onChange={captureTask}
           value={nameTask}
         />
         <button className={styleButton}>Add Task</button>
@@ -33,7 +45,21 @@ const Tasks = () => {
 
       <ul>
         {arrayTasks.map((task) => (
-          <li key={task}> {task}</li>
+          <li
+            className="text-lg font-medium p-1"
+            // onClick={() => console.log(task)}
+            key={task.name}
+            style={
+              task.complete === true ? { textDecoration: "line-through" } : {}
+            }
+          >
+            {task.name}
+            {task.complete === false ? (
+              <button onClick={() => uploadTask(task)} className={styleButton}>
+                Complete
+              </button>
+            ) : null}
+          </li>
         ))}
       </ul>
     </>
