d1fcd872ec1240f2d7bc2b3cec4016ba4cc8225b
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-08-26 20:46:16 -0400

    Feat: Components and useState

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const DbPersonal = [
+    {
+        id: "1",
+        nombre: "Verena Alvarez",
+        correo: "colaborador1@colaborador1.com"
+    },
+    {
+        id: "2",
+        nombre: "Alejandro Gonzalez",
+        correo: "colaborador2@colaborador2.com"
+    },
+    {
+        id: "3",
+        nombre: "Sebastian Oyarzo",
+        correo: "colaborador3@colaborador3.com"
+    },
+    {
+        id: "4",
+        nombre: "Francisco Nuñez",
+        correo: "colaborador4@colaborador4.com"
+    }
+]
