311cf9c351b9ac9a9153d097eac7359086f3f0bd
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-08-28 23:52:39 -0400

    Feat: Submit preventDefault

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 
 function Form() {
+  const submitForm = (e) => {
+    console.log(e)
+    return e.preventDefault()
+  }
+
   return (
     <form>
       <div className="mb-6">
@@ -32,10 +37,10 @@ function Form() {
           placeholder="Ingrese email"
           required=""
         />
-        
       </div>
 
       <button
+        onClick={submitForm}
         type="submit"
         className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
